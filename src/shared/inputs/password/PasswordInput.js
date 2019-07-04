fce1893a063900476b521d59cd0491236ee69e73
Eduard Dolynskyi <eduard.dolinskyi@binary-studio.com>
2019-07-04 21:34:45 +0300

    convert class component Password to functional component

--- src/shared/inputs/password/PasswordInput.js
+++ src/shared/inputs/password/PasswordInput.js
@@ -1,38 +1,22 @@
-import React from 'react';
+import React, { useState }from 'react';
 import PropTypes from 'prop-types';
 
-class PasswordInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isShown: false
-        };
-        this.onClick = this.onClick.bind(this);
-    }
+const PasswordInput = ({ text, keyword, label, onChange }) => {
+    const [isShown, setIsShown] = useState(false);
+    const inputType = isShown ? 'text' : 'password';
 
-    onClick() {
-        this.setState({
-            isShown: !this.state.isShown
-        });
-    }
-
-    render() {
-        const props = this.props;
-        const inputType = this.state.isShown ? 'text' : 'password';
-
-        return (
-            <div className="form-group row">
-                <label className="col-sm-3 col-form-label">{ props.label }</label>
-                <input
-                    className="col-sm-7"
-                    value={ props.text }
-                    type={ inputType }
-                    onChange={ e => props.onChange(e, props.keyword) }
-                />
-                <button className="col-sm-2" onClick={ this.onClick }>&#x1f441;</button>
-            </div>
-        );
-    }
+    return (
+        <div className="form-group row">
+            <label className="col-sm-3 col-form-label">{ label }</label>
+            <input
+                className="col-sm-7"
+                value={ text }
+                type={ inputType }
+                onChange={ e => onChange(e, keyword) }
+            />
+            <button className="col-sm-2" onClick={() => setIsShown(!isShown) }>&#x1f441;</button>
+        </div>
+    );
 }
 
 PasswordInput.propTypes = {
